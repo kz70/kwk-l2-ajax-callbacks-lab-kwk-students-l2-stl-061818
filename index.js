85bd03dc7c0240db87176ca13490f01973994d44
Kailin Zhang <kailin11511@gmail.com>
2018-06-26 15:53:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 $(document).ready(function (){
 });
+
+let url;
+let input
+$('#submitButton').click(function(){
+    input = $('#searchText').val()
+    console.log(input)
+    searchRepositories(input)
+})
+
+
+function searchRepositories(searchTerms){
+    $.ajax({
+        url: 'https://api.github.com/search/repositories?q=',
+        dataType: 'json',
+        data: {
+          q: input
+          
+        },
+        success: resp => {
+          url 
+        }
+      })
+}
